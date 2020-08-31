2471b7c9b690160cacb0e89b227c8d1a97fce724
syukrontanzilah <syukrontanzilah@gmail.com>
2020-08-31 16:21:36 +0700

    add package

---
+++ src/Page/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Aplikasi Menu</Text>
+            <Ionicons name="heart" size={30} />
+        </View>
+    )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
